0b432c649d2dc27ca7cf122ed06ff24fd9d7b3cf
lijunlong <lijunlong@openresty.com>
2023-08-12 15:08:58 +0800

    tests: upgrade nginx to 1.25.1.

--- util/build.sh
+++ util/build.sh
@@ -8,19 +8,20 @@ version=$1
 force=$2
 
 ngx-build $force $version \
-            --with-cc-opt="-I$PCRE_INC -I$OPENSSL_INC" \
-            --with-ld-opt="-L$PCRE_LIB -L$OPENSSL_LIB -Wl,-rpath,$SREGEX_LIB:$PCRE_LIB:$LIBDRIZZLE_LIB:$OPENSSL_LIB" \
-        --with-http_ssl_module \
-            --without-mail_pop3_module \
-            --without-mail_imap_module \
-            --without-mail_smtp_module \
-            --without-http_upstream_ip_hash_module \
-            --without-http_empty_gif_module \
-            --without-http_memcached_module \
-            --without-http_referer_module \
-            --without-http_autoindex_module \
-            --without-http_auth_basic_module \
-            --without-http_userid_module \
+          $NGX_EXTRA_OPT \
+          --with-cc-opt="-I$PCRE_INC -I$OPENSSL_INC" \
+          --with-ld-opt="-L$PCRE_LIB -L$OPENSSL_LIB -Wl,-rpath,$SREGEX_LIB:$PCRE_LIB:$LIBDRIZZLE_LIB:$OPENSSL_LIB" \
+          --with-http_ssl_module \
+          --without-mail_pop3_module \
+          --without-mail_imap_module \
+          --without-mail_smtp_module \
+          --without-http_upstream_ip_hash_module \
+          --without-http_empty_gif_module \
+          --without-http_memcached_module \
+          --without-http_referer_module \
+          --without-http_autoindex_module \
+          --without-http_auth_basic_module \
+          --without-http_userid_module \
           --add-module=$root $opts \
           --add-module=$root/../ndk-nginx-module \
           --add-module=$root/../lua-nginx-module \
